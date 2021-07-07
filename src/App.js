de955a13c5ee4b1b97a03ad7634c1f540772ca85
mohammedabed <37789022+mohammedabed@users.noreply.github.com>
2021-07-07 23:46:19 +0300

    p

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { MainContainer } from "./Global.Styles";
 import Nav from "./Components/Nav/Nav";
 import HomeScreen from "./Screen/HomeScreen/HomeScreen";
 import MovieScreen from "./Screen/MovieScreen/MovieScreen";
+import {Route, Switch} from "react-router";
 
 function App() {
   return (
@@ -9,6 +10,10 @@ function App() {
       <Nav />
       {/*
       <HomeScreen />*/}
+        <Switch>
+            <Route  exact={true} path={"/"} component={HomeScreen}/>
+            <Route  exact={true} path={"/movie/:id"} component={MovieScreen}/>
+        </Switch>
       <MovieScreen />
     </MainContainer>
   );
